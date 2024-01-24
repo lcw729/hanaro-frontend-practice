8372d442aff83e4b6e439ad87c3c9f1f20200575
lcw729 <lcw729729@gmail.com>
2024-01-24 17:46:50 +0900

    :sparkles: JS 변수 선언하기

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+// primitive(call-by-value) vs object(call-by-reference)
+const userName = 'Hong'; // string (primitive)
+var age; // declare + define (undefined)
+console.log("🚀 ~ age:", age)
+age = 33;
+console.log(`Hello, ${userName}!`)
