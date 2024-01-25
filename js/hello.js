8e7035db77f50d81b0e5ef77fc7896cec42d2cc9
lcw729 <lcw729729@gmail.com>
2024-01-25 17:32:18 +0900

    :pencil2: TDZ, freshness 학습

--- js/hello.js
+++ js/hello.js
@@ -4,3 +4,12 @@ var age; // declare + define (undefined)
 console.log("🚀 ~ age:", age)
 age = 33;
 console.log(`Hello, ${userName}!`)
+
+// console.log(zz);
+// ReferenceError: zz is not defined
+// let zz = <NotYetInitialized>; // TDZ => 무력화!(Freshness)
+ 
+let zz;
+zz = 9; // 이 라인 실행 시 암묵적 var! globalThisValue
+console.log(zz);
+// console.log(globalThis(zz)); // OK
