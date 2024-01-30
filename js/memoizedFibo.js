57abcae0ad1b51901854d7c37582946459fb8fb2
lcw729 <lcw729729@gmail.com>
2024-01-30 16:46:49 +0900

    :tada: javascript closure 피보나치 문제풀이

--- js/memoizedFibo.js
+++ js/memoizedFibo.js
@@ -11,31 +11,38 @@ const testfibo = function(n) {
 function memoized(fn) {
     const memoizedTable = {}
     console.log(memoizedTable)
-    return function B(n) {
+    return function(n) {
         return memoizedTable[n] || (memoizedTable[n] = fn(n))
     }
 }
 
-function fibo(n) {
+function makefibo(n) {
     console.log('this is n: ', n);
     return memoized(function (n) {
-        runCnt += 1
+        runCnt += 1;
         if (n === 0) return 0;
         else if (n === 1) return 1;
         else return fibo(n - 1) + fibo(n - 2);
     })
 }
 
-const memoizedFibo2 = fibo(2);
+const fibo = memoized(function (n) {
+    runCnt += 1;
+    if (n === 0) return 0;
+    else if (n === 1) return 1;
+    else return fibo(n - 1) + fibo(n - 2);
+});
+
+const memoizedFibo2 = fibo(2); // f(2) -> f(1) + f(0)
 console.log("🚀 ~ memoizedFibo2:", memoizedFibo2, runCnt)
 
 runCnt = 0;
-const memoizedFibo5 = fibo(5);
+const memoizedFibo5 = fibo(5); // f(5) -> f(4) + f(3)
 console.log("🚀 ~ memoizedFibo5:", memoizedFibo5, runCnt)
 
 runCnt = 0;
-const memoizedFibo7 = fibo(7);
+const memoizedFibo7 = fibo(7); // f(7), f(6) 2회 호출
 console.log("🚀 ~ memoizedFibo7:", memoizedFibo7, runCnt)
 
-const fibo5 = testfibo(5)
-console.log('fibo5 : ', fibo5)
+// const fibo5 = testfibo(5)
+// console.log('fibo5 : ', fibo5)
