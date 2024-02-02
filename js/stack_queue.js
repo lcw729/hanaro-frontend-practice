d1ae96b97a25ffe1fe97ff4d2668b1babe406052
lcw729 <lcw729729@gmail.com>
2024-02-02 09:54:48 +0900

    :pushpin: javascript Stack, Queue 구현

---
+++ js/stack_queue.js
@@ -0,0 +1,61 @@
+class Collection {
+    #arr;
+    constructor(...args) { // [a, b, c]  a, b, c
+        this.#arr = Array.isArray(args[0]) ? args[0] : args;
+    }
+
+    get _arr() {
+        return this.#arr;
+    }
+
+    push(value) {
+        this.#arr.push(value);
+        return this;
+    }
+
+    size() {
+        return this.#arr?.length;
+    }
+
+    toString() {
+        return `${this.constructor.name}(${this.size()})${JSON.stringify(this.#arr)}`;
+    }
+
+    print() {
+        console.log(this.toString());
+    }
+}
+
+class Stack extends  Collection{
+
+    constructor(...args) { // - 생략 가능
+        super(...args);
+    }
+
+    pop() {
+        return this._arr.pop();
+    }
+
+}
+
+class Queue extends Collection{
+
+    enqueue(value) {
+        this.push(value);
+        return this;
+    }
+
+    dequeue(){
+        return this._arr.shift();
+    }
+}
+
+const stack = new Stack([1, 2]);
+stack.print();
+stack.push(3).push(5)
+console.log('last pop=', stack.pop());
+
+const queue = new Queue();
+queue.enqueue(10).enqueue(20);
+queue.print();
+console.log('last queue=', queue.dequeue());
