45ddc82443480e0035d800f94a4ceacc4d666c2e
lcw729 <lcw729729@gmail.com>
2024-02-07 17:17:58 +0900

    :pencil2: Typescript 인덱스 시그니처 학습

---
+++ ts/index.ts
@@ -0,0 +1,26 @@
+type Novel = {
+    [key: string]: string | number | boolean | {id: number} | undefined;
+    id: number;
+    title: string;
+    addr: {id: number};
+    isFree?: boolean;
+} & {itemNo: number};
+
+let book : Novel = {id: 1, title: 'IQ84', addr: {id: 300}, itemNo: 33};
+
+type X = {
+    id: number;
+    name: string;
+}
+
+type Y = {id: number, addr: string};
+type Z = X | Y;
+const compos: Z = {id:1, name: 'Hong', addr: 'Seoul'}; // 잘못된 예 compos.id; 밖에 사용못함.
+
+type nZ = {
+    [i: string]: string | number;
+    id: number;
+} & Y;
+
+const ncompos: nZ = {id:1, name: 'Hong', addr: 'Seoul'};
+console.log(ncompos.addr);
