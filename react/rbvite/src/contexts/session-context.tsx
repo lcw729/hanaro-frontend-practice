0e67ee20abf521ade03b119c0f80836fa5b583b8
lcw729 <lcw729729@gmail.com>
2024-03-29 06:46:33 +0900

    :recycle: React: session - usereducer 리팩토링

--- react/rbvite/src/contexts/session-context.tsx
+++ react/rbvite/src/contexts/session-context.tsx
@@ -1,5 +1,6 @@
 // context 생성
 import { createContext, PropsWithChildren, useContext, useReducer } from 'react';
+import { LoginUser } from '../App.tsx';
 
 type Session = {
   loginUser: LoginUser | null;
@@ -34,52 +35,53 @@ const SessionContext = createContext<sessionContextProps>({
 });
 
 type ReducerAction = {
-  action: 'login',
-  payload: {id: number, name: string}
-} | {
-  action: 'logout'
-  payload: null
+  action: 'login' | 'logout',
+  payload: LoginUser | null
 } | {
   action: 'removeItem',
-  payload: {id: number}
+  payload: number
 } | {
   action: 'saveItem',
-  payload: {name: string, price: number}
+  payload: Cart
 }
 
-const reducer = (session: Session, {action, payload}:ReducerAction ): Session => {
-  let newSession: Session
+/*
+괄호를 넣어야할 때, isCartType과 같이 payload 타입체크로 묶어 주기도 한다.
+function isCartType(data: Cart | number): data is Cart {
+  return typeof data !== 'number';
+}
+ */
+
+const reducer = (session: Session, { action, payload }: ReducerAction): Session => {
+  let newSession: Session;
   switch (action) {
-    case "login": {
-      const {id, name} = payload;
-      newSession = {...session, loginUser: {id, name}};
-      return newSession; }
-    case "logout":
-      return defaultSession;
-    case "removeItem":
-      const newCart = session.cart.filter((item) => payload.id !== item.id);
-      newSession = {...session, cart: newCart};
+    case 'login':
+    case 'logout':
+      return { ...session, loginUser: payload };
+    case 'removeItem':
+      newSession = { ...session, cart: session.cart.filter((item) => payload !== item.id) };
       return newSession;
-    case "saveItem": {
+    case 'saveItem': {
+      const { cart } = session;
       // strictMode로 두번 실행된다. => 해결하기 위해서 메모리에 직접 접근
-      const { name, price } = payload;
-      let max = 0;
-      session.cart.forEach((item) => {
-        if (item.id > max) max = item.id});
-      const item: Cart = {id: max+1, name, price };
+      const {name, price } = payload;
+      const maxId = Math.max(...session.cart.map((item) => item.id), 0);
+      // const item: Cart = { id: maxId + 1, name, price };
       // 문제 발생 !! 얕은 복사로 newCart와 session.cart의 주소가 같다.
       // strictMode로 두번 실행 시, 아이템이 두개씩 추가되는 문제가 발생한다.
       // const newCart = session.cart;
 
       // 깊은 복사로 매번 새로운 값을 추가하고, 기존의 Heap에 존재하는 cart는 건들지 못하도록 한다.
-      const newCart = session.cart.map((item) => {return { ...item }});
+      const newCart = session.cart.map((item) => {
+        return { ...item };
+      });
       console.log(session.cart === newCart); // false
-      newCart.push(item);
-      newSession = { ...session, cart: newCart };
+      // newCart.push(item);
+      newSession = { ...session, cart: [...cart, { id: maxId + 1, name, price }] };
       return newSession;
     }
   }
-}
+};
 
 // provider 생성
 export const SessionProvider = ({ children }: PropsWithChildren) => {
@@ -87,25 +89,25 @@ export const SessionProvider = ({ children }: PropsWithChildren) => {
   const [session, dispatch] = useReducer(reducer, defaultSession);
 
   const login = (id: number, name: string) => {
-    dispatch({action: 'login', payload: {id, name}});
+    dispatch({ action: 'login', payload: { id, name } });
   };
 
   const logout = () => {
-    dispatch({action: 'logout', payload: null});
+    dispatch({ action: 'logout', payload: null });
   };
 
   const removeItem = (id: number) => {
-    dispatch({action: 'removeItem', payload: {id}});
-  }
+    dispatch({ action: 'removeItem', payload: id });
+  };
 
 
   const saveItem = (name: string, price: number) => {
-    dispatch({action: 'saveItem', payload: {name, price}});
-  }
+    dispatch({ action: 'saveItem', payload: { name, price } as Cart });
+  };
 
   return (
     <>
-      <SessionContext.Provider value={{session, login, logout, removeItem, saveItem}}>
+      <SessionContext.Provider value={{ session, login, logout, removeItem, saveItem }}>
         {children}
       </SessionContext.Provider>
     </>
